7b41b98697486076d70b6beda22a4df8ad66529a
Pol Roca Llaberia <pol.roca@mango.com>
2022-02-24 21:19:45 +0100

    add pupil and group classes

--- lib/core/main.dart
+++ lib/core/main.dart
@@ -1,37 +1,52 @@
-class Pupil {
-  List<Pupil> should;
-  List<Pupil> shouldNot;
-  List<Pupil> must;
-  List<Pupil> mustNot;
-  Group group;
+import 'package:csv/csv.dart';
+//import 'package:file_picker/file_picker.dart';
+import 'dart:io';
 
-  get getShould => this.should;
+class Pupil {
+  String name;
+  List<Pupil>? should;
+  List<Pupil>? shouldNot;
+  List<Pupil>? must;
+  List<Pupil>? mustNot;
+  Group? group;
+
+  Pupil(this.name);
+
+  get getShould => should;
 
   set setShould(List<Pupil> should) => this.should = should;
 
-  get getShouldNot => this.shouldNot;
+  get getShouldNot => shouldNot;
 
   set setShouldNot(shouldNot) => this.shouldNot = shouldNot;
 
-  get getMust => this.must;
+  get getMust => must;
 
   set setMust(must) => this.must = must;
 
-  get getMustNot => this.mustNot;
+  get getMustNot => mustNot;
 
   set setMustNot(mustNot) => this.mustNot = mustNot;
 
-  get getGroup => this.group.getName();
+  get getGroup => group?.getName;
 
-  set setGroup(group) => this.group = group;
+  set setGroup(Group group) => this.group = group;
 }
 
 class Group {
   String name;
 
-  String get getName => this.name;
+  Group(this.name);
+
+  get getName => name;
 
   set setName(String name) => this.name = name;
 }
 
-main() {}
+//test main
+main() {
+  var p = Pupil('Miguel Corcho');
+  p.group = Group('Perlana');
+
+  print(p.group);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/main.dart';
 
 void main() {
   runApp(const MyApp());
